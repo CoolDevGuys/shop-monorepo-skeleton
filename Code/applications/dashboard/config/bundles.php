30e84205d5317280166f576acb69049970a5151c
Alejandro Romero <ealejandrorg@gmail.com>
2022-01-17 01:45:12 +0100

    Add missing sentry config

--- Code/applications/dashboard/config/bundles.php
+++ Code/applications/dashboard/config/bundles.php
@@ -5,5 +5,6 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     FriendsOfBehat\SymfonyExtension\Bundle\FriendsOfBehatSymfonyExtensionBundle::class => ['test' => true],
+    Sentry\SentryBundle\SentryBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
 ];
